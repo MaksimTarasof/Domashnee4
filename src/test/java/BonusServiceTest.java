30a807dd201c0ffae7c6cdfec43517ef4551444d
Max <eparis@atlassian.com>
2023-01-11 15:11:01 +0400

    second commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -25,6 +25,55 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
+        long expected = 1000;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateForRegisteredAndOver() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1500;
+        boolean registered = true;
+        long expected = 45;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateForRegisteredAndOve() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 999;
+        boolean registered = true;
+        long expected = 29;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateForRegisteredAndO() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 5_000_000;
+        boolean registered = true;
         long expected = 500;
 
         // вызываем целевой метод:
@@ -32,6 +81,24 @@ public class BonusServiceTest {
 
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
+
     }
 
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateForRegisteredAnO() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 100;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
 }
+
